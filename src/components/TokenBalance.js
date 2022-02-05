d599320fac54390c7e721993b317acbda11a0bc1
Feodor Hilitski <fhilitski@gmail.com>
2022-02-05 16:34:39 -0500

    changed TokenBalace component to class

--- src/components/TokenBalance.js
+++ src/components/TokenBalance.js
@@ -1,14 +1,41 @@
-import {Button, FloatingLabel, Form, Row, Col} from 'react-bootstrap'
-import {useEffect, useState} from 'react';
+import {Button, FloatingLabel, Form, Alert} from 'react-bootstrap'
+import React from 'react';
 
-function TokenBalance (props) {
+class TokenBalance extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      isLoading : false,
+      contractAddress : "0x0d8775f648430679a709e98d2b0cb6250d2887ef", //undefined,
+      tokenBalance : undefined,
+      errorMsg : undefined
+    }
 
-  const [isLoading, setLoading] = useState(false);
-  const handleClick = () => setLoading(true);
+    this.account = props.account;
+    this.web3 = props.web3; 
+    this.minABI = [{
+      // The minimum ABI required to get the ERC20 Token balance
+      // balanceOf ERC-20 function
+        constant: true,
+        inputs: [{ name: "_owner", type: "address" }],
+        name: "balanceOf",
+        outputs: [{ name: "balance", type: "uint256" }],
+        type: "function"
+    }];
+    
+    this.getBalance = this.getBalance.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  
+    const BATtokenAddress = "0x0d8775f648430679a709e98d2b0cb6250d2887ef"; 
+    //BAT contract is 0x0d8775f648430679a709e98d2b0cb6250d2887ef 
+  }
 
-  let account = props.account;
-  let web3 = props.web3;
-  const minABI = [
+  //const [isLoading, setLoading] = useState(false);
+  //const handleClick = () => setLoading(true);
+
+  //let account = props.account;
+  //let web3 = props.web3;
+  /*const minABI = [
     // The minimum ABI required to get the ERC20 Token balance
     // balanceOf ERC-20 function
     {
@@ -18,11 +45,9 @@ function TokenBalance (props) {
       outputs: [{ name: "balance", type: "uint256" }],
       type: "function"
     }];
-
-  const BATtokenAddress = "0x0d8775f648430679a709e98d2b0cb6250d2887ef"; //BAT contract is 0x0d8775f648430679a709e98d2b0cb6250d2887ef 
-  let tokenAddress = BATtokenAddress;
-
-  async function getBalance(ethAccount, abi, tokenAddress) {
+  */
+  
+  async getBalance(web3, ethAccount, abi, tokenAddress) {
     let contract = new web3.eth.Contract(abi, tokenAddress);
     let result = undefined;
     let errorMsg = "";
@@ -30,28 +55,31 @@ function TokenBalance (props) {
       result = await contract.methods.balanceOf(ethAccount).call();
     }
     catch (e) {
-      errorMsg = "Error getting token balance: " + e;
+      errorMsg = e.toString();
       console.log(errorMsg);
     }
     let formatedBalance = (result !== undefined) ? web3.utils.fromWei(result) : 0 ;
-    return {formatedBalance,errorMsg};
+    return {formatedBalance, errorMsg};
   }
-
-  useEffect(() => {
-    if (isLoading) {  
-     
-        getBalance(account,minABI, BATtokenAddress ).then((res) => {
-        setLoading(false);
-        console.log(res.formatedBalance);
-        console.log(res.errorMsg);
-      });
-    }
-  }, [isLoading]);
-    
+  
+  handleClick() {
+    console.log('click!');
+    console.log(this.props.web3);
+    this.setState({isLoading : true});  
+    this.getBalance(this.props.web3, this.props.account, this.minABI, this.state.contractAddress).then((res) => {
+      console.log(res.formatedBalance);
+      console.log(res.errorMsg);
+      this.setState({isLoading : false, tokenBalance : res.formatedBalance, errorMsg : res.errorMsg});
+    })
+    ;
+  }
+  
+  render() {
   let retElement =
     <div id = "tokenBalanceChecker" className = "top-margin">
-
-      <Form onSubmit={handleClick}>
+      <Form
+        // onSubmit={this.handleClick}
+      >
       <FloatingLabel
           controlId="floatingInput"
           label="Contract address"
@@ -62,13 +90,23 @@ function TokenBalance (props) {
       <Button
         type="submit"
         variant="primary"
-        disabled={isLoading}
-        onClick={!isLoading ? handleClick : null}
+        disabled={this.state.isLoading}
+        onClick={!this.state.isLoading ? this.handleClick : null}
       >
-        {isLoading ? 'Loading…' : 'Get token balance'}
+        {this.state.isLoading ? "Loading…" : "Get token balance"}
       </Button>
       </Form>
+      <br/>
+      <div id="tokenBalanceOutput">
+        {(this.state.errorMsg === undefined || this.state.errorMsg === "") ? 
+        ((this.state.tokenBalance !== undefined) ? "Token balance: " + this.state.tokenBalance : "") 
+        : 
+        (<Alert variant="danger"> {this.state.errorMsg} </Alert>)
+        }
+      </div>
     </div>
-  return (retElement)
+    return (retElement)
+  }
 }
+
 export {TokenBalance}
